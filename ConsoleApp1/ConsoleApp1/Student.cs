949c7d53c2366ba5b55ce72cc4ee6baf2f8cc302
dominhnhien1997 <nhienminhdo@gmail.com>
2017-11-30 00:05:59 +0700

    them mot class  moi la student

---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public float Mark{ get; set; }
+        public Student()
+        {
+
+        }
+        public Student(string id,string name,float mark)
+        {
+            Id = id;
+            Name = name;
+            Mark = mark;
+        }
+        public override string ToString()
+        {
+            return Id + "---" + Name + "---" + Mark;
+        }
+    }
+}
