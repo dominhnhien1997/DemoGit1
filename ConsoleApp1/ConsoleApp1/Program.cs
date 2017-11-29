a849f346bec1e19da4eb5e30458b14e0c0971d1c
dominhnhien1997 <nhienminhdo@gmail.com>
2017-11-29 23:52:18 +0700

    lần đầu làm chuyện ấy

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xin chao");
+            Console.WriteLine("thuc hien phep cong");
+            int a, b;
+            Console.WriteLine("nhap a");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("nhap b");
+            b = Convert.ToInt16(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("tong cua hai so la :"+c);
+            Console.ReadKey();
+        }
+    }
+}
