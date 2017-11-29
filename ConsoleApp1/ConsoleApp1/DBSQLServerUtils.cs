84b20f352fcc64a3bca9e61542ff25414c2ee644
dominhnhien1997 <nhienminhdo@gmail.com>
2017-11-30 00:11:58 +0700

    ket noi co so du lieu

---
+++ ConsoleApp1/ConsoleApp1/DBSQLServerUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DBSQLServerUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string datasource = @"tran-vmware\SQLEXPRESS";
+
+            string database = "simplehr";
+            string username = "sa";
+            string password = "1234";
+
+            return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
+        }
+    }
+}
